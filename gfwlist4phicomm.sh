2303ccb4e17e66bedad1d1941cbe14b819d2efeb
Neil Steven <neil-steven@hotmail.com>
2018-04-06 08:43:20 +0800

    First commit

---
+++ gfwlist4phicomm.sh
@@ -0,0 +1,112 @@
+#/bin/sh
+
+# gfwlist4phicomm.sh
+#
+# Update the gfwlist for an Phicomm router with unofficial ROM.
+#
+# Fork from https://github.com/cokebar/gfwlist2dnsmasq
+# Modified by Neil Steven
+#
+# Update on 2018-04-06 09:00
+
+DNS_IP='93.46.8.89'
+
+clean_and_exit(){
+    # Clean up temp files
+    printf 'Cleaning up... '
+    rm -rf $TMP_DIR
+    printf 'Done\n\n'
+    [ $1 -eq 0 ] && printf 'Job Finished. It will take effect soon.\n\n' || printf 'Exit with Error code '$1'.\n'
+    exit $1
+}
+
+check_depends(){
+    which sed base64 curl >/dev/null
+    if [ $? != 0 ]; then
+        printf 'Error: Missing Dependency.\nPlease check whether you have the following binaries on you system:\nsed, base64, curl.\n'
+        exit 3
+    fi
+}
+
+process(){
+    # Set Global Var
+    BASE_URL='https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
+    TMP_DIR="/tmp/gfwlist4phicomm"
+    BASE64_FILE="$TMP_DIR/base64.txt"
+    GFWLIST_FILE="$TMP_DIR/gfwlist.txt"
+    DOMAIN_FILE="$TMP_DIR/gfwlist2domain.txt"
+    CONF_TMP_FILE="$TMP_DIR/gfwlist.conf.tmp"
+    OUT_TMP_FILE="$TMP_DIR/gfwlist.out.tmp"
+    OUT_FILE="/root/ssr/gfw_addr.conf"
+
+    if [ ! -d $TMP_DIR ];then
+        mkdir $TMP_DIR
+    fi
+
+    # Fetch gfwlist and decode it into plain text
+    printf 'Fetching gfwlist... '
+    curl -s -L -k -o $BASE64_FILE $BASE_URL
+    if [ $? != 0 ]; then
+        printf '\nFailed to fetch gfwlist. Please check your Internet connection.\n'
+        clean_and_exit 2
+    fi
+    base64 -d $BASE64_FILE > $GFWLIST_FILE || ( printf 'Failed to decode gfwlist.txt. Quit.\n'; clean_and_exit 2 )
+    printf 'Done.\n\n'
+
+    # Convert
+    IGNORE_PATTERN='^\!|\[|^@@|(https?://){0,1}[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
+    HEAD_FILTER_PATTERN='s#^(\|\|?)?(https?://)?##g'
+    TAIL_FILTER_PATTERN='s#/.*$|%2F.*$##g'
+    DOMAIN_PATTERN='([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)'
+    HANDLE_WILDCARD_PATTERN='s#^(([a-zA-Z0-9]*\*[-a-zA-Z0-9]*)?(\.))?([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)(\*)?#\4#g'
+
+    printf 'Converting gfwlist to dnsmasq rules ...\n' 
+    printf '\nWARNING:\nThe following lines in GfwList contain regex, and might be ignored:\n\n'
+    cat $GFWLIST_FILE | grep -n '^/.*$'
+    printf "\nThis script will try to convert some of the regex rules. But you should know this may not be a equivalent conversion.\nIf there's regex rules which this script do not deal with, you should add the domain manually to the list.\n\n"
+    grep -vE $IGNORE_PATTERN $GFWLIST_FILE | sed -r $HEAD_FILTER_PATTERN | sed -r $TAIL_FILTER_PATTERN | grep -E $DOMAIN_PATTERN | sed -r $HANDLE_WILDCARD_PATTERN > $DOMAIN_FILE
+
+    # Add Google domains
+    printf 'google.com\ngoogle.ad\ngoogle.ae\ngoogle.com.af\ngoogle.com.ag\ngoogle.com.ai\ngoogle.al\ngoogle.am\ngoogle.co.ao\ngoogle.com.ar\ngoogle.as\ngoogle.at\ngoogle.com.au\ngoogle.az\ngoogle.ba\ngoogle.com.bd\ngoogle.be\ngoogle.bf\ngoogle.bg\ngoogle.com.bh\ngoogle.bi\ngoogle.bj\ngoogle.com.bn\ngoogle.com.bo\ngoogle.com.br\ngoogle.bs\ngoogle.bt\ngoogle.co.bw\ngoogle.by\ngoogle.com.bz\ngoogle.ca\ngoogle.cd\ngoogle.cf\ngoogle.cg\ngoogle.ch\ngoogle.ci\ngoogle.co.ck\ngoogle.cl\ngoogle.cm\ngoogle.cn\ngoogle.com.co\ngoogle.co.cr\ngoogle.com.cu\ngoogle.cv\ngoogle.com.cy\ngoogle.cz\ngoogle.de\ngoogle.dj\ngoogle.dk\ngoogle.dm\ngoogle.com.do\ngoogle.dz\ngoogle.com.ec\ngoogle.ee\ngoogle.com.eg\ngoogle.es\ngoogle.com.et\ngoogle.fi\ngoogle.com.fj\ngoogle.fm\ngoogle.fr\ngoogle.ga\ngoogle.ge\ngoogle.gg\ngoogle.com.gh\ngoogle.com.gi\ngoogle.gl\ngoogle.gm\ngoogle.gp\ngoogle.gr\ngoogle.com.gt\ngoogle.gy\ngoogle.com.hk\ngoogle.hn\ngoogle.hr\ngoogle.ht\ngoogle.hu\ngoogle.co.id\ngoogle.ie\ngoogle.co.il\ngoogle.im\ngoogle.co.in\ngoogle.iq\ngoogle.is\ngoogle.it\ngoogle.je\ngoogle.com.jm\ngoogle.jo\ngoogle.co.jp\ngoogle.co.ke\ngoogle.com.kh\ngoogle.ki\ngoogle.kg\ngoogle.co.kr\ngoogle.com.kw\ngoogle.kz\ngoogle.la\ngoogle.com.lb\ngoogle.li\ngoogle.lk\ngoogle.co.ls\ngoogle.lt\ngoogle.lu\ngoogle.lv\ngoogle.com.ly\ngoogle.co.ma\ngoogle.md\ngoogle.me\ngoogle.mg\ngoogle.mk\ngoogle.ml\ngoogle.com.mm\ngoogle.mn\ngoogle.ms\ngoogle.com.mt\ngoogle.mu\ngoogle.mv\ngoogle.mw\ngoogle.com.mx\ngoogle.com.my\ngoogle.co.mz\ngoogle.com.na\ngoogle.com.nf\ngoogle.com.ng\ngoogle.com.ni\ngoogle.ne\ngoogle.nl\ngoogle.no\ngoogle.com.np\ngoogle.nr\ngoogle.nu\ngoogle.co.nz\ngoogle.com.om\ngoogle.com.pa\ngoogle.com.pe\ngoogle.com.pg\ngoogle.com.ph\ngoogle.com.pk\ngoogle.pl\ngoogle.pn\ngoogle.com.pr\ngoogle.ps\ngoogle.pt\ngoogle.com.py\ngoogle.com.qa\ngoogle.ro\ngoogle.ru\ngoogle.rw\ngoogle.com.sa\ngoogle.com.sb\ngoogle.sc\ngoogle.se\ngoogle.com.sg\ngoogle.sh\ngoogle.si\ngoogle.sk\ngoogle.com.sl\ngoogle.sn\ngoogle.so\ngoogle.sm\ngoogle.sr\ngoogle.st\ngoogle.com.sv\ngoogle.td\ngoogle.tg\ngoogle.co.th\ngoogle.com.tj\ngoogle.tk\ngoogle.tl\ngoogle.tm\ngoogle.tn\ngoogle.to\ngoogle.com.tr\ngoogle.tt\ngoogle.com.tw\ngoogle.co.tz\ngoogle.com.ua\ngoogle.co.ug\ngoogle.co.uk\ngoogle.com.uy\ngoogle.co.uz\ngoogle.com.vc\ngoogle.co.ve\ngoogle.vg\ngoogle.co.vi\ngoogle.com.vn\ngoogle.vu\ngoogle.ws\ngoogle.rs\ngoogle.co.za\ngoogle.co.zm\ngoogle.co.zw\ngoogle.cat\n' >> $DOMAIN_FILE
+    printf 'Google search domains... Added\n'
+
+    # Add blogspot domains
+    printf 'blogspot.ca\nblogspot.co.uk\nblogspot.com\nblogspot.com.ar\nblogspot.com.au\nblogspot.com.br\nblogspot.com.by\nblogspot.com.co\nblogspot.com.cy\nblogspot.com.ee\nblogspot.com.eg\nblogspot.com.es\nblogspot.com.mt\nblogspot.com.ng\nblogspot.com.tr\nblogspot.com.uy\nblogspot.de\nblogspot.gr\nblogspot.in\nblogspot.mx\nblogspot.ch\nblogspot.fr\nblogspot.ie\nblogspot.it\nblogspot.pt\nblogspot.ro\nblogspot.sg\nblogspot.be\nblogspot.no\nblogspot.se\nblogspot.jp\nblogspot.in\nblogspot.ae\nblogspot.al\nblogspot.am\nblogspot.ba\nblogspot.bg\nblogspot.ch\nblogspot.cl\nblogspot.cz\nblogspot.dk\nblogspot.fi\nblogspot.gr\nblogspot.hk\nblogspot.hr\nblogspot.hu\nblogspot.ie\nblogspot.is\nblogspot.kr\nblogspot.li\nblogspot.lt\nblogspot.lu\nblogspot.md\nblogspot.mk\nblogspot.my\nblogspot.nl\nblogspot.no\nblogspot.pe\nblogspot.qa\nblogspot.ro\nblogspot.ru\nblogspot.se\nblogspot.sg\nblogspot.si\nblogspot.sk\nblogspot.sn\nblogspot.tw\nblogspot.ug\nblogspot.cat\n' >> $DOMAIN_FILE
+    printf 'Blogspot domains... Added\n'
+
+    # Add twimg.edgesuite.net
+    printf 'twimg.edgesuite.net\n' >> $DOMAIN_FILE
+    printf 'twimg.edgesuite.net... Added\n'
+
+    # Convert domains into dnsmasq rules
+    sort -u $DOMAIN_FILE | sed -r 's#(.+)#address=/.\1/'$DNS_IP'#g' > $CONF_TMP_FILE
+
+    # Generate output file
+    echo '# dnsmasq rules generated by gfwlist for unofficial Phicomm ROMs' >> $OUT_TMP_FILE
+    echo '# https://github.com/Neil-Steven/gfwlist4phicomm' >> $OUT_TMP_FILE
+    echo "# Last Updated on $(date "+%Y-%m-%d %H:%M:%S")" >> $OUT_TMP_FILE
+    echo '# ' >> $OUT_TMP_FILE
+    cat $CONF_TMP_FILE >> $OUT_TMP_FILE
+
+    if [ -f $OUT_FILE ];then
+        BACKUP_FILE="${OUT_FILE}.$(date '+%Y%m%d-%H%M%S').bak"
+        mv $OUT_FILE $BACKUP_FILE
+    fi
+
+    cp $OUT_TMP_FILE $OUT_FILE
+    printf '\nConverting gfwlist to dnsmasq rules... Done\n\n'
+
+    # Restart SSR (drop all output)
+    nohup /root/ssrcmd.sh restart >/dev/null 2>&1 &
+
+    # Clean up
+    clean_and_exit 0
+}
+
+main() {
+    check_depends
+    printf '\nJob Started.\n\n'
+    process
+}
+
+main "$@"
